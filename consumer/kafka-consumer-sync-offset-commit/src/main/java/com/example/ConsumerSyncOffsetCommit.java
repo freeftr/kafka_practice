007dcc02ebaaa1f6043361333269e7881cb625c1
freeftr <jonghaparkdevelop@gmail.com>
2025-02-27 20:22:16 +0900

    Feat: kafka-consumer-sync-offset-commit

---
+++ consumer/kafka-consumer-sync-offset-commit/src/main/java/com/example/ConsumerSyncOffsetCommit.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class ConsumerSyncOffsetCommit {
+    private final static Logger logger = LoggerFactory.getLogger(ConsumerSyncOffsetCommit.class);
+    private final static String TOPIC = "sync_offset_commit";
+    private final static String BOOTSTRAP_SERVERS = "kafka:9092";
+    private final static String GROUP_ID = "group_id";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
+        consumer.subscribe(Arrays.asList(TOPIC));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            Map<TopicPartition, OffsetAndMetadata> currentOffset = new HashMap<>();
+
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info(record.topic() + ":" + record.partition() + ":" + record.offset());
+                /**
+                 * 개별 레코드 단위로 커밋 진행
+                 */
+                currentOffset.put(
+                        new TopicPartition(record.topic(), record.partition()),
+                        new OffsetAndMetadata(record.offset() + 1, null)
+                );
+                consumer.commitSync(currentOffset);
+            }
+        }
+    }
+}
